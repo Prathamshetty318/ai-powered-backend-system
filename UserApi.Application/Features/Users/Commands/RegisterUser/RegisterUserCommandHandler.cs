f118a0af5eb3ba2b3618ee975109c3389fdfb6d6
Pratham <shettypratham73@gmail.com>
2026-07-14 20:25:44 +0530

    created command and handler so that every action has its own handler ratger tha huge userservice

---
+++ UserApi.Application/Features/Users/Commands/RegisterUser/RegisterUserCommandHandler.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UserApi.Domain.Entities;
+using MediatR;
+using UserApi.Domain.Interfaces;
+
+namespace UserApi.Application.Features.Users.Commands.RegisterUser
+{
+    public class RegisterUserCommandHandler : IRequestHandler<RegisterUserCommand, int>
+    { 
+        private readonly IUnitOfWork _unitOfWork;
+
+        public RegisterUserCommandHandler(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public async Task<int> Handle (RegisterUserCommand request, CancellationToken cancellationToken)
+        {
+
+        }
+    }
+}
