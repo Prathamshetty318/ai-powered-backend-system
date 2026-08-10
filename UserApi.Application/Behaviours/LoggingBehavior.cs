7fbbddd7f1addd8827eca25132c7e5d316ba2b44
Pratham <shettypratham73@gmail.com>
2026-08-10 22:03:17 +0530

    Changed the code and done for Logger as well

--- UserApi.Application/Behaviours/LoggingBehavior.cs
+++ UserApi.Application/Behaviours/LoggingBehavior.cs
@@ -4,19 +4,27 @@ using System.Linq;
 using System.Threading.Tasks;
 using MediatR;
 using System.Text;
+using Microsoft.Extensions.Logging;
 
 namespace UserApi.Application.Behaviours
 {
     public class LoggingBehavior<TRequest, TResponse> :
         IPipelineBehavior<TRequest, TResponse>
     {
+
+        private readonly ILogger<LoggingBehavior<TRequest, TResponse>> _logger;
+
+        public LoggingBehavior(ILogger<LoggingBehavior<TRequest, TResponse>> logger)
+        {
+            _logger = logger;
+        }
         public async Task<TResponse> Handle(TRequest request, CancellationToken cancellationToken, RequestHandlerDelegate<TResponse> next)
         {
-            Console.WriteLine($"Handling : {typeof(TRequest).Name}");
+            _logger.LogInformation("Handling {RequestName}",typeof(TRequest).Name);
 
             var Response = await next();
 
-            Console.WriteLine($"Handled : {typeof(TRequest).Name}");
+            _logger.LogInformation("Handled {RequestName}", typeof(TRequest).Name);
 
             return Response;
         }
